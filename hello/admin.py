495f61036e0ac4208da8564c2181e009f7c18a82
Hassan-Mallah <hassansmallah@gmail.com>
2020-04-26 22:31:44 +0300

    commit message

--- hello/admin.py
+++ hello/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Greeting
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Greeting)
+admin.site.register(SearchResult)
+admin.site.register(SearchAreas)
+admin.site.register(TelegramUser)
--- hello/models.py
+++ hello/models.py
@@ -6,3 +6,22 @@ class Greeting(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class SearchResult(models.Model):
+    query = models.CharField(max_length=200)
+    result = models.CharField(max_length=400)
+    query_date = models.DateTimeField(auto_now_add=True)
+
+
+class TelegramUser(models.Model):
+    telegram_id = models.IntegerField()
+    result_id = models.ForeignKey(SearchResult, on_delete=models.CASCADE)
+
+
+class SearchAreas(models.Model):
+    area = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.area
+
