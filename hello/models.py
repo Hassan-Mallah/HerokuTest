0b01c3661e9d7a313bf333b3db98fb5b2eb06d02
Hassan-Mallah <hassansmallah@gmail.com>
2020-04-26 20:25:50 +0300

    commit message

--- hello/models.py
+++ hello/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
